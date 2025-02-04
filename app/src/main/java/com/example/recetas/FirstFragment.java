868802d4a784ecd1bafbb8d9ce2a153bdb6f1e37
=Alberto <jlr0013@alu.medac.es>
2025-02-04 14:28:19 +0100

    Añadido fragment 5

--- app/src/main/java/com/example/recetas/FirstFragment.java
+++ app/src/main/java/com/example/recetas/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.recetas;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +11,8 @@ import android.widget.TableRow;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 
 public class FirstFragment extends Fragment {
@@ -19,30 +20,25 @@ public class FirstFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Cargar la vista del diseño del fragment
         View view = inflater.inflate(R.layout.fragment_first, container, false);
+
         // Obtener el TableRow y asignar el listener
         TableRow tableRow1 = view.findViewById(R.id.tableRow1);
-        tableRow1.setOnClickListener(v -> onTableRowClick());
-        return view;
+        tableRow1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verRecetaEnsalada(v);
+            }
+        });
+
+        return view; // Asegúrate de retornar la vista
     }
     // Este método se ejecutará al hacer clic en el TableRow
-    public void onTableRowClick() {
-        // Crear el Fragment5 y navegar hacia él
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        fragment5 fragment5 = new fragment5();
-
-        // Pasar datos al Fragment5 (por ejemplo, nombre de la receta)
-        Bundle bundle = new Bundle();
-        bundle.putString("recipe_name", "Nombre de la receta");
-        fragment5.setArguments(bundle);
-
-        // Reemplazar el fragmento actual por Fragment5
-        transaction.replace(R.id.fragmentContainerView, fragment5);
-        transaction.addToBackStack(null); // Opcional, para que el fragmento sea apilado en la pila de retroceso
-        transaction.commit();
+    public void verRecetaEnsalada(View view) {
+        NavController navController = Navigation.findNavController(view);
+        navController.navigate(R.id.fragment5);
     }
 
 }
